3d28384822ff7e61e0e87135f1f3497838c7c356
Jason Mason <jasonjaymason@gmail.com>
2023-05-15 18:08:01 -0600

    Added Mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 import { readdirSync } from 'node:fs'
+import { mdsvex } from 'mdsvex'
+import * as path from 'path'
+import { fileURLToPath } from 'node:url'
 
 function getPrerenderEntries() {
   let prerenderFolders = ['./src/content/company-pages/']
@@ -20,14 +23,29 @@ function getPrerenderEntries() {
   })
 }
 
+//Get the root directory 
+const dirname = path.resolve(fileURLToPath(import.meta.url), '../');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
 
-  preprocess: preprocess({
-    postcss: true
-  }),
+  extensions: ['.svelte', '.svelte.md', '.md', '.svx'],
+  preprocess: [
+    preprocess({
+      postcss: true
+    }),
+    mdsvex({
+      extensions: ['.md', '.svx'],
+      layout: {
+        legal: path.join(dirname, './src/lib/components/svx-layouts/legal.svelte')
+      },
+      smartypants: {
+        dashes: 'oldschool'
+      }
+    })
+  ],
 
   kit: {
     adapter: adapter(),
