44a018f469bf6a8b2d0dcfdfa638198502ca61e3
Jason Mason <jasonjaymason@gmail.com>
2023-05-15 18:08:01 -0600

    Added Mdsvex

---
+++ src/lib/components/svx-layouts/legal.svelte
@@ -0,0 +1,10 @@
+<script>
+	//SVX layouts must be in js and cannot be ts
+	import Button from '../Button.svelte';
+	let blah = 'From template';
+</script>
+
+<div>
+	<h1>{blah}</h1>
+	<slot />
+</div>
